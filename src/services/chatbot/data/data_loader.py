ad181e9cdbdc0ee6a892d041150181469c07e53e
StaticOwl <staticowl@nomad>
2023-10-17 00:29:16 -0400

    Updating Dataloader

--- src/services/chatbot/data/data_loader.py
+++ src/services/chatbot/data/data_loader.py
@@ -1,19 +1,37 @@
 import json
+import os
 from pytorch_pretrained_bert import cached_path
+
+# url = "https://s3.amazonaws.com/datasets.huggingface.co/personachat/personachat_self_original.json"
 
 
 def load_data_to_json(url=None, file_path=None):
     data = None
     if url:
         file_path = cached_path(url)
-        with open(file_path, "r", encoding="utf-8") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
             data = json.loads(f.read())
-    elif file_path:
-        with open(file_path, "r", encoding="utf-8") as f:
-            data = json.loads(f.read())
+    if data:
+        return data
+    else:
+        raise Exception("No input source provided.")
 
-url = "https://s3.amazonaws.com/datasets.huggingface.co/personachat/personachat_self_original.json"
-personachat_file = cached_path(url)
 
-with open(personachat_file, "r", encoding="utf-8") as f:
-    data = json.loads(f.read())
+def process_input_json(dataset = None, data_types = ['train', 'valid']):
+    if not dataset:
+        raise Exception("Dataset is empty.")
+    os.makedirs("runtime", exist_ok=True)
+    for data_type in data_types:
+        input_data = dataset[data_type]
+        output_data = []
+
+        for i, datum in enumerate(input_data):
+            output_data.append({
+                "id": f"{i:0>5}",
+                **datum
+            })
+        with open(f"runtime/{data_type}_data.json", "w", encoding="utf-8") as f:
+            for datum in output_data:
+                line = json.dumps(datum, ensure_ascii=False)
+                print(line, file=f)
+
