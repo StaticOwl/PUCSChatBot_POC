c41ba75656d05f66b92afb518d05d2fad4032da8
Sanidhya Sharma <sanidhyarsharma@gmail.com>
2023-11-26 16:19:05 -0500

    Switched chat UI to Modal view

--- frontend/pfw_chatbot/src/App.js
+++ frontend/pfw_chatbot/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
-import ChatUI from './ChatUI';
-import Navbar from './Navbar'
+import { useState } from "react";
+import "./App.css";
+import Modal from "./Components/Modal";
+import Navbar from "./Components/Navbar";
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
   return (
     <div className="App">
       <Navbar></Navbar>
@@ -12,11 +14,13 @@ function App() {
           <p>Welcome to PFW, Computer Science Department! </p>
         </div>
       </div>
-      <div className="ChatBox">
-        <ChatUI></ChatUI>
-      </div>
+      <button className="ChatButton" onClick={() => {
+        setShowModal(true);
+      }}>Chat with us!</button>
+      {showModal && <Modal hideModal={setShowModal} />}
     </div>
   );
 }
 
 export default App;
+ 
