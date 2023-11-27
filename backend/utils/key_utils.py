80b87bdd3af75d87fc928b54ee79051b19b04b93
Koustav Mallick <24458780+StaticOwl@users.noreply.github.com>
2023-11-26 19:13:19 -0500

    Merge pull request #21
    * enabling auto setup for project
    
    * Updated Readme
    
    * Updated Readme
    
    * master snc
    
    * Updating the setup code with fallbacks
    
    * Merge branch 'main' of github.com:StaticOwl/PFW_ChatBot into mallk01
    
    * creating dataloader boilerplate
    
    * Updating Dataloader
    
    * Data Preprocessing Init
    
    * updating the recursive scrapping file in order to write another scrap…
    
    * Merge branch 'main' of https://github.com/StaticOwl/PFW_ChatBot into …
    
    * Sync with Master
    
    * updated setup to work as is and added keystroke stop util code, can b…
    
    * Added few more utils
    
    * Restructured a bit
    
    * Restructured a bit
    
    * Added Formal Code
    
    * Refactor
    
    * Added Temp Scraping Code
    
    * updated readme
    
    * Added Archived Code for reference
    
    * adding files to test actions
    
    * adding files to test actions
    
    * adding files to test actions
    
    * updated the workflow location
    
    * updated the workflow location
    
    * updated the workflow location
    
    * updated the workflow location
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * updated the workflow
    
    * Changing the main code
    
    * Changing the main code
    
    * Changing the main code
    
    * Changing the main code
    
    * Updating Test Workflow
    
    * Updating Test Workflow
    
    * Merge remote-tracking branch 'origin/mallk01' into mallk01
    
    * Updating Test Workflow
    
    * Updating Test Workflow
    
    * Merge remote-tracking branch 'origin/main' into mallk01
    
    * Adding Scrapping Code
    
    * Updated Configs and made changes for facultylist and profile configs
    
    * Deleted Unnecessary File
    
    * Removed Data Scraping Archived Code as no need, and also added some s…
    
    * Updated datascrapping to take along the catalog data based on catalog…
    
    * Merge branch 'main' of https://github.com/StaticOwl/PFW_ChatBot into …
    
    * Updated datascrapping to take along the catalog data based on catalog…
    
    * Removed redundant files
    
    * Updating Github Actions
    
    * Updating GitHub Actions
    
    * Updating GitHub Actions
    
    * Updating GitHub Actions
    
    * Updating GitHub Actions
    
    * Updating GitHub Actions
    
    * Added more relevant docstrings
    
    * Added more relevant docstrings
    
    * actions
    
    * actions

--- backend/utils/key_utils.py
+++ backend/utils/key_utils.py
@@ -1,21 +1,27 @@
-""" I am going to use the keyboard module to listen for the keypresses and then
+""" I am going to use the keyboard module to listen for the key presses and then
 use that to stop any ongoing loop. This will help on logging the errors safely and properly,
 otherwise KeyboardInterrupt usually gives off a lot of garbage errors."""
 
 import subprocess
 import sys
+
 subprocess.check_call([sys.executable, "-m", "pip", "install", "keyboard"], stdout=subprocess.DEVNULL,
                       stderr=subprocess.DEVNULL)
 import keyboard
 import queue
-import threading
 
 key_to_press = "esc"
 
 
-def wait_for_key(e, key_queue):
+def wait_for_key(e, keypress_queue):
+    """
+    This method is used to put the key in the queue if it is pressed.
+    :param e: key pressed.
+    :param keypress_queue: queue to put the key in.
+    :return: queue with the bool if matches.
+    """
     if e.name == key_to_press:
-        key_queue.put(True)
+        keypress_queue.put(True)
 
 
 key_queue = queue.Queue()
@@ -23,6 +29,10 @@ keyboard.on_press(lambda e: wait_for_key(e, key_queue))
 
 
 def stop_key_listener():
+    """
+    This method is used to stop the key listener.
+    :return: True if key is pressed, False otherwise.
+    """
     if not key_queue.empty():
         print(f"{key_to_press} pressed. Stopping setup...")
         return True
