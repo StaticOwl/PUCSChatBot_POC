1a94e221f585ff4ea9a32ab43018f407ed1ec075
StaticOwl <akostuv@gmail.com>
2023-12-04 01:30:40 -0500

    Updating ReadMe to show the final structure of the Project (As of Now)

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     sys.path.append('.')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train', '-t', action='store_true', help='train the model')
     parser.add_argument('--setup', '-s', action='store_true', help='setup the project')
     parser.add_argument('--scraping', '-sc', action='store_true', help='scraping the data from list of urls')
 
